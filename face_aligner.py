88e1e112fa90c8f58bc0af4ebdb6f282f5e35554
Kim Ninh Huynh <kimninh1610@gmail.com>
2020-10-24 08:36:26 +0700

    update README, provide shape_preditor file

--- face_aligner.py
+++ face_aligner.py
@@ -1,45 +1,42 @@
-import PIL
-import bz2
-from keras.utils import get_file
+import PIL.Image
+import os
+
 from .ffhq_dataset.face_alignment import image_align
 from .ffhq_dataset.landmarks_detector import LandmarksDetector
 
-def unpack_bz2(src_path):
-  data = bz2.BZ2File(src_path).read()
-  dst_path = src_path[:-4]
-  with open(dst_path, 'wb') as fp:
-    fp.write(data)
-  return dst_path  
 
 class FaceAligner:
-  def __init__(self, 
-               output_size = 256,
-               x_scale=1,
-               y_scale=1,
-               em_scale=0.1,
-               use_alpha=False):
-    LANDMARKS_MODEL_URL = 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
-    landmarks_model_path = unpack_bz2(get_file('shape_predictor_68_face_landmarks.dat.bz2',
-                                               LANDMARKS_MODEL_URL, cache_subdir='temp'))
-    self.landmarks_detector = LandmarksDetector(landmarks_model_path)
-    self.output_size = output_size
-    self.x_scale = x_scale
-    self.y_scale = y_scale
-    self.em_scale = em_scale
-    self.alpha = use_alpha
+    def __init__(self,
+                 output_size=256,
+                 x_scale=1,
+                 y_scale=1,
+                 em_scale=0.1,
+                 use_alpha=False):
+        shape_predictor_dir = os.path.dirname(os.path.abspath(__file__))
+        self.landmarks_detector = LandmarksDetector(
+            os.path.join(shape_predictor_dir, 'shape_predictor_68_face_landmarks.dat'))
+        self.output_size = output_size
+        self.x_scale = x_scale
+        self.y_scale = y_scale
+        self.em_scale = em_scale
+        self.alpha = use_alpha
 
-  def align(self, image: PIL.Image) -> PIL.Image:
-    face_landmarks_list = self.landmarks_detector.get_landmarks(image)
-    
-    if len(face_landmarks_list) == 0 or len(face_landmarks_list) > 1:
-      return null
+    def align(self, image: PIL.Image) -> (PIL.Image, str):
+        face_landmarks_list = self.landmarks_detector.get_landmarks(image)
 
-    face_landmarks = face_landmarks_list[0]
-    return image_align(image,
-                face_landmarks, 
-                output_size=self.output_size, 
-                x_scale=self.x_scale, 
-                y_scale=self.y_scale, 
-                em_scale=self.em_scale, 
-                alpha=self.alpha)
+        if len(face_landmarks_list) == 0:
+            return None, 'No face detected'
 
+        if len(face_landmarks_list) > 1:
+            return None, 'More than one face found'
+
+        face_landmarks = face_landmarks_list[0]
+        aligned_image = image_align(image,
+                                    face_landmarks,
+                                    output_size=self.output_size,
+                                    x_scale=self.x_scale,
+                                    y_scale=self.y_scale,
+                                    em_scale=self.em_scale,
+                                    alpha=self.alpha)
+
+        return aligned_image, None
